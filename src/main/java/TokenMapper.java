6890043c59d97ed6f3b59ed79bdaa117907ce8b2
Jeremy <jeremy.herbst111@gmail.com>
2021-03-03 23:19:21 +0100

    Standard 0.0.1

--- src/main/java/TokenMapper.java
+++ src/main/java/TokenMapper.java
@@ -1,39 +1,21 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 // Mapper <Input Key, Input Value, Output Key, Output Value>
 public class TokenMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
-    private final Text word = new Text();
-    private LinkedList<String> hashtagCount= new LinkedList<>();
+    private Text word = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-
-        Parser parser = new Parser();
-            try
-            {
-                hashtagCount = parser.getHashtags(value.toString());
-            }
-            catch (ParseException e)
-            {
-                e.printStackTrace();
-            }
-            if(hashtagCount.size() > 0)
-            {
-                for (String hashtag : hashtagCount)
-                {
-                    if (hashtag != null)
-                    {
-                        word.set(hashtag);
-                        context.write(word, one);
-                    }
-                }
-            }
+        StringTokenizer itr = new StringTokenizer(value.toString());
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
         }
+    }
 }
